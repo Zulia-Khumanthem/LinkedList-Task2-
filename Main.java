b9e83b844ec296a0f879090390e2bae75adf456c
Zulia Khumanthem <116053936+Zulia-Khumanthem@users.noreply.github.com>
2025-04-29 22:14:26 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        list.insert(18);
+        list.insert(25);
+        list.insert(45);
+        list.insert(12);
+
+        list.show();
+    }
+}
